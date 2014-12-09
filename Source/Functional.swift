089285762de5d07662919162d86f8f77d2fb3586
Tony DiPasquale <tony@thoughtbot.com>
2014-12-09 15:14:35 -0500

    Change bind operator to be internal

--- Source/Functional.swift
+++ Source/Functional.swift
@@ -15,7 +15,7 @@ func <*><A, B>(f: (A -> B)?, a: A?) -> B? {
 }
 
 infix operator >>- { associativity left precedence 150 }
-public func >>-<A, B>(a: A?, f: A -> B?) -> B? {
+func >>-<A, B>(a: A?, f: A -> B?) -> B? {
     switch a {
     case let .Some(x): return f(x)
     case .None: return .None
